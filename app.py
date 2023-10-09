3b17d6901e61036c48f0be4f5bded090d6df98b9
maehoo99 <maehoo99.gmail.com>
2023-10-09 10:43:28 +0800

    Updated with valid cert instead of bypassing via api_request.py in OpenAI package

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import os
+
+os.environ["REQUESTS_CA_BUNDLE"] = "/users/YEEPG/cacert.pem"
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 
 def main():
     load_dotenv()
